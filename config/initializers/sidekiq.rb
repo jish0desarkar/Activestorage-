fe1f47d5a641c95f48210f38ba88a1808eb09c2e
jish0desarkar <desarkarjishnu@gmail.com>
2020-03-12 17:39:45 +0530

    fix: sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,20 @@
-Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://localhost:6379/0'  }
-  end
+# Sidekiq.configure_server do |config|
+#     config.redis = { url: 'redis://localhost:6379/0'  }
+#   end
   
-  Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://localhost:6379/0'  }
-  end
+#   Sidekiq.configure_client do |config|
+#     config.redis = { url: 'redis://localhost:6379/0'  }
+#   end
+
+redis_url = ENV['REDIS_URL']
+
+Sidekiq.configure_server do |config|
+config.redis = {
+:namespace => 'workers'
+}
+end
+Sidekiq.configure_client do |config|
+config.redis = {
+:namespace => 'workers'
+}
+end
